3e54ce0a960f14772de3941760389b50e63b34f2
Rbox <rbox2@proton.me>
2023-08-16 17:08:48 +0330

    add task limit

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <time.h>
 
 #define MAX_SIZE 10
+#define TASK_LIMIT 100
 
 
 struct Task {
-    int id; 
     void* (*fn)(void*) ;
 }; 
 
@@ -22,6 +22,9 @@ struct Queue {
     pthread_mutex_t mutex;
     pthread_cond_t notify;
 };
+
+int shutdown = 0;
+int task_count = 0;
 
 struct Queue Q;
 
@@ -40,24 +43,34 @@ worker(void* args) {
         // be retested before the mutex lock is acquired. The recommended test 
         // method is to write the condition check as a while() loop that calls 
         // pthread_cond_wait().
-
-        while (Q.count == 0) {
+        while (Q.count == 0 && shutdown != 1) {
             if ((pthread_cond_wait(&Q.notify,&Q.mutex)) != 0) {
                 perror("worker: failed to wait\n");
                 exit(0);
             }
         } 
+
+        if (shutdown == 1) {
+           break; 
+        }
+
         struct Task t = Q.task_queue[Q.head];
         Q.head = (Q.head + 1) % MAX_SIZE;
         Q.count--;
+        task_count++;
+        int task_id = task_count;
         if ((pthread_mutex_unlock(&Q.mutex)) != 0 ) {
             perror("worker: failed to unlock mutex\n");
             exit(0);
         }
-        t.fn((void*)&t.id);
+        t.fn((void*)&task_id);
     }
-
+    if ((pthread_mutex_unlock(&Q.mutex)) != 0 ) {
+        perror("worker: failed to unlock mutex\n");
+        exit(0);
+    }
     pthread_exit(NULL);
+    return NULL;
 }
 
 void*
@@ -80,7 +93,7 @@ insert_task() {
         }
         return;
     }
-    struct Task t = {rand()%100,task};
+    struct Task t = {task};
     Q.task_queue[Q.tail] = t;
     Q.tail = ( Q.tail + 1 ) % MAX_SIZE;
     Q.count++;
@@ -118,11 +131,21 @@ main() {
     }
 
 
+
     while (1) {
         insert_task();
+        pthread_mutex_lock(&Q.mutex);
+        if (task_count >= TASK_LIMIT) {
+            shutdown = 1;
+            pthread_cond_broadcast(&Q.notify);
+            pthread_mutex_unlock(&Q.mutex);
+            break;
+        }
+        pthread_mutex_unlock(&Q.mutex);
     }
 
     for (int i = 0 ; i < MAX_SIZE; i++) {
         pthread_join(thread_pool[i].t,NULL);
     }
+
 }
